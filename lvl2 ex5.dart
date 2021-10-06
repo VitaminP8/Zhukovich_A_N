40ea5392ae69bc1ef7c164d48f9b56fd4bed2e60
Alexey <alexey.zhukovich@gmail.com>
2021-10-06 12:19:46 +0300

    lvl2 ex4

---
+++ lvl2 ex5.dart
@@ -0,0 +1,15 @@
+import 'dart:io';
+
+void main() {
+
+  int n = int.parse(stdin.readLineSync()!);
+  List<int> a = [];
+  int x = 4;
+  for (int i = 0; i < n; i++){
+    a.add(x);
+    x = x + 3;
+  }
+  for (int i = 0; i < n; i++){
+    print (a[i]);
+  }
+}
